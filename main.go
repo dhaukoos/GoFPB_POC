4bd6b1a61c088d1f9b0de66a7fa703a7d437b74c
Dana Haukoos <dana.haukoos1@accesscfa.com>
2024-11-11 08:57:11 -0500

    Create Example1 as seperate file

--- main.go
+++ main.go
@@ -1,39 +1,6 @@
 package main
 
-import (
-    "context"
-    "fmt"
-)
-import "time"
-
 func main() {
 	println("Hello World")
-    HelloGoFBP2()
-}
-
-/*
-* A minimalist FBP example with a single output emitter node (type int->),
-* a single input and output processor node (int->int),
-* and a single input receiver node (->int).
-*/
-
-func HelloGoFBP() {
-    channel1 := make(chan int, 2)
-    channel2 := make(chan int, 2)
-    ctx, cancel := context.WithCancel(context.Background())
-
-    goEmitter(channel1, ctx)
-    goProcessor(channel1, channel2, func(i int) int { return i * i }, ctx)
-    goReceiver(channel2, func(i int) { fmt.Printf("Received here %v\n", i) }, ctx)
-
-    time.Sleep(200 * time.Millisecond)
-
-    // Cancel all goroutines
-    fmt.Printf("Canceling emitJob\n")
-    fmt.Printf("Canceling processJob\n")
-    fmt.Printf("Canceling receiveJob\n")
-    cancel()
-
-    // Wait for the worker to finish (optional)
-    time.Sleep(time.Second)
+    HelloGoFBP3()
 }
